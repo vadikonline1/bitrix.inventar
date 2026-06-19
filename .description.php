a5db1e49cdd58c6c093dfd222b6155b519500873
vadikonline1 <vadikonline1@gmail.com>
2026-06-19 09:38:07 +0300

    Create .description.php

---
+++ .description.php
@@ -0,0 +1,12 @@
+<?php
+
+use Bitrix\Main\Localization\Loc;
+
+Loc::loadMessages(__FILE__);
+
+$arModuleDescription = [
+    "NAME" => "IT Inventory",
+    "DESCRIPTION" => "Manage workstations, printers, monitors, laptops and other company equipment",
+    "PARTNER_NAME" => "vadikonline1",
+    "PARTNER_URI" => "https://github.com/vadikonline1/bitrix.inventar/",
+];
